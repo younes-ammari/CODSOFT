d3c5a38eec3f62c8e70ed4bb489e05b4f8fe5200
Younes Ammari <72352932+younes-ammari@users.noreply.github.com>
2024-03-13 01:06:31 +0100

    Calculator code

---
+++ TASK 02 - Calculator/main.py
@@ -0,0 +1,48 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
+
+def calculator():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    
+    print("Welcome to Simple Calculator!")
+    print("Choose operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input")
+
+
+if __name__ == "__main__":
+    calculator()
